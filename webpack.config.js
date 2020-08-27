04b2db19040ba0b3442589b24dd7b1a3b9dd1a4a
adam allalou <aabou9@gmail.com>
2020-08-27 08:51:57 +0100

    add styles files

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,28 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+       {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader',
+         ],
+       },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader',
+         ],
+       }
+    ],
+  },
 };
